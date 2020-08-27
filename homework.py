c52d5a4d1dc1c0c001597e5b68795290ad060b9e
Anatoliy <leo-testarossa@yandex.ru>
2020-08-27 18:17:03 +0300

    Second review

--- homework.py
+++ homework.py
@@ -5,25 +5,24 @@ class Calculator:
     def __init__(self, limit):
         self.limit = limit
         self.records = []
+        self.today = dt.date.today()
+        self.week_ago = self.today - dt.timedelta(days=6)
 
     def add_record(self, records):
         self.records.append(records)
 
     def get_today_stats(self):
-        self.total_spent = 0
-        for i in self.records:
-            if i.date == dt.date.today():
-                self.total_spent += i.amount
-        return self.total_spent
+        return sum(i.amount for i in self.records if i.date == self.today)
+
+    def get_today_spent(self):
+        return self.limit - self.get_today_stats()
 
     def get_week_stats(self):
-        self.week_spent = 0
-        week_ago = dt.date.today() - dt.timedelta(days=7)
-        today = dt.date.today()
+        week_spent = 0
         for i in self.records:
-            if today >= i.date >= week_ago:
-                self.week_spent += i.amount
-        return self.week_spent
+            if self.today >= i.date >= self.week_ago:
+                week_spent += i.amount
+        return week_spent
 
 
 class CashCalculator(Calculator):
@@ -31,30 +30,30 @@ class CashCalculator(Calculator):
     EURO_RATE = 70.0
     USD_RATE = 60.0
     RUB_RATE = 1
-    distonary_rate = {EURO_RATE: 'eur', USD_RATE: 'usd', RUB_RATE: 'rub'}
-    rate_name = {'rub': 'руб', 'eur': 'Euro', 'usd': 'USD'}
+    dict_rate = {'eur': [EURO_RATE, 'Euro'],
+                 'usd': [USD_RATE, 'USD'],
+                 'rub': [RUB_RATE, 'руб']}
 
     def get_today_cash_remained(self, current):
-        remainder = abs(self.limit - self.get_today_stats())
-        for i in self.distonary_rate:
-            surname = self.rate_name[self.distonary_rate[i]]
-            if self.distonary_rate[i] == current:
-                if self.limit > self.get_today_stats():
-                    return f'На сегодня осталось {round(remainder/i, 2)} {surname}'
-                elif self.limit < self.get_today_stats():
-                    return f'Денег нет, держись: твой долг - {round((remainder/i), 2)} {surname}'
-                else:
-                    return ('Денег нет, держись')
+        remainder = self.get_today_spent()
+        today_stats = self.get_today_stats()
+        course, rate = self.dict_rate[current]
+        total_spent = abs(remainder / course)
+        if self.limit > today_stats:
+            return f'На сегодня осталось {total_spent:.2f} {rate}'
+        elif self.limit < today_stats:
+            return f'Денег нет, держись: твой долг - {total_spent:.2f} {rate}'
+        return 'Денег нет, держись'
 
 
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
-        spent_calories = super().get_today_stats()
-        more_calories = self.limit - spent_calories
+        more_calories = self.get_today_spent()
+        spent_calories = self.get_today_stats()
         if self.limit >= spent_calories:
-            return f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {more_calories} кКал'
-        else:
-            return f'Хватит есть!'
+            return (f'Сегодня можно съесть что-нибудь ещё,'
+                    f' но с общей калорийностью не более {more_calories} кКал')
+        return 'Хватит есть!'
 
 
 class Record:
@@ -69,7 +68,8 @@ class Record:
 
 if __name__ == '__main__':
     cash_calculator = CashCalculator(1000)
-    cash_calculator.add_record(Record(amount=145, comment="кофе")) 
-    cash_calculator.add_record(Record(amount=300, comment="Серёге за обед"))
-    cash_calculator.add_record(Record(amount=3000, comment="бар в Танин др", date="08.11.2019"))
-    print(cash_calculator.get_today_cash_remained("rub"))
+    cash_calculator.add_record(Record(amount=145, comment='кофе'))
+    cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
+    cash_calculator.add_record(Record(amount=3000, comment='бар в Танин др',
+                                      date='08.11.2019'))
+    print(cash_calculator.get_today_cash_remained('rub'))
